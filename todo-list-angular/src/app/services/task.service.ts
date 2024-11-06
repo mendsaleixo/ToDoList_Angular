3ce66dcc4da18f18b6e10f7de3aedeac825fb140
Viviane Balbino <mithisu@gmail.com>
2024-11-06 14:21:37 -0300

    implementar metodos CRUD utilizando a API CRUDCRUD

---
+++ todo-list-angular/src/app/services/task.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  private apiUrl = 'https://crudcrud.com/api/4691c3badbc24c309aacc2cdd39967c2/tasks'; 
+
+  constructor(private http: HttpClient) {}
+
+
+  getTasks(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  addTask(task: any): Observable<any> {
+    return this.http.post(this.apiUrl, task);
+  }
+
+  updateTask(id: string, task: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, task);
+  }
+
+  deleteTask(id: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+}
