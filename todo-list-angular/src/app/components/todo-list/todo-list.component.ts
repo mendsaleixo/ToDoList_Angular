f5e4c783188a748fa53618a671b54eca52abc640
Viviane Balbino <mithisu@gmail.com>
2024-11-08 18:29:54 -0300

    implementar json server

--- todo-list-angular/src/app/components/todo-list/todo-list.component.ts
+++ todo-list-angular/src/app/components/todo-list/todo-list.component.ts
@@ -9,63 +9,54 @@ import { TaskService } from 'src/app/services/task.service';
 export class TodoListComponent implements OnInit {
   tasks: any[] = [];
   showTaskForm = false;
-  taskToEdit: any = null; 
+  taskToEdit: any = null;
+
   constructor(private taskService: TaskService) {}
 
   ngOnInit(): void {
     this.loadTasks();
   }
 
-  
   loadTasks(): void {
     this.taskService.getTasks().subscribe(tasks => {
       this.tasks = tasks;
     });
   }
 
- 
   addTask(): void {
-    debugger
     this.showTaskForm = true;
-    this.taskToEdit = null; 
+    this.taskToEdit = null;
   }
 
-  
   saveTask(task: any): void {
-    debugger
     if (this.taskToEdit) {
-      delete task._id;
-      this.taskService.updateTask(this.taskToEdit._id, task).subscribe(() => {
+      this.taskService.updateTask(this.taskToEdit.id, task).subscribe(() => {
         this.loadTasks(); 
         this.showTaskForm = false;
-        this.taskToEdit = null; 
+        this.taskToEdit = null;
       });
     } else {
-      debugger
-        this.taskService.addTask(task).subscribe(() => {
+      this.taskService.addTask(task).subscribe(() => {
         this.loadTasks(); 
         this.showTaskForm = false;
       });
     }
   }
 
- 
   editTask(index: number): void {
     this.taskToEdit = { ...this.tasks[index] }; 
     this.showTaskForm = true; 
   }
 
-  
   removeTask(index: number): void {
-    const taskId = this.tasks[index]._id;
+    const taskId = this.tasks[index].id;
     this.taskService.deleteTask(taskId).subscribe(() => {
       this.loadTasks();  
     });
   }
 
-  
   cancelTaskForm(): void {
     this.showTaskForm = false;
-    this.taskToEdit = null; 
+    this.taskToEdit = null;
   }
 }
--- todo-list-angular/src/app/services/task.service.ts
+++ todo-list-angular/src/app/services/task.service.ts
@@ -6,24 +6,27 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class TaskService {
-  private apiUrl = 'https://crudcrud.com/api/d7e8ea3f6e62485ab8e27971468ac4ab/tasks'; 
+  private apiUrl = 'http://localhost:3000/tasks';
 
   constructor(private http: HttpClient) {}
-
 
   getTasks(): Observable<any[]> {
     return this.http.get<any[]>(this.apiUrl);
   }
 
+  getTaskById(id: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${id}`);
+  }
+
   addTask(task: any): Observable<any> {
-    return this.http.post(this.apiUrl, task);
+    return this.http.post<any>(this.apiUrl, task);
   }
 
-  updateTask(id: string, task: any): Observable<any> {
-    return this.http.put(`${this.apiUrl}/${id}`, task);
+  updateTask(id: number, task: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/${id}`, task);
   }
 
-  deleteTask(id: string): Observable<any> {
-    return this.http.delete(`${this.apiUrl}/${id}`);
+  deleteTask(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/${id}`);
   }
 }
